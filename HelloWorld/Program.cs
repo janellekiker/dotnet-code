7355d2af0a4fe88cdffca1eb164ba58de44b8de5
Janelle Kiker <janellekiker@gmail.com>
2023-06-01 08:52:43 -0500

    learned WriteLine

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// Console.Write("First");
+// Console.Write("Second");
+
+using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
