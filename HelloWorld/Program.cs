75f3c67449f8143d59fe1f43f3e06a8e9d81f172
Janelle Kiker <janellekiker@gmail.com>
2023-06-13 16:13:53 -0500

    loops and methods covered

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,13 +8,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int[] intsToCompress = new int[] {10, 15, 20, 25, 30, 12, 34};
-
-            int totalValue = intsToCompress[0] + intsToCompress[1]
-                + intsToCompress[2] + intsToCompress[3]
-                + intsToCompress[4] + intsToCompress[5]
-                + intsToCompress[6];
-
+            int firstNumber = 5;
+            int secondNumber = 6;
+            int thirdNumber = 4;
+            Console.WriteLine(firstNumber * secondNumber * thirdNumber);
         }
     }
 }
