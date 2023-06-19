f9c77c68233b98e6ce3ed0b3ec35d6ba4fcc53f6
Janelle Kiker <janellekiker@gmail.com>
2023-06-19 14:15:30 -0500

    reviewed models and learning how to connect database

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using System.Data;
+using Dapper;
+using HelloWorld.Models;
+using Microsoft.Data.SqlClient;
 
 namespace HelloWorld
 {
@@ -8,10 +12,32 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int firstNumber = 5;
-            int secondNumber = 6;
-            int thirdNumber = 4;
-            Console.WriteLine(firstNumber * secondNumber * thirdNumber);
+            // Connect to Database
+            string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=false;User Id=SA;Password=SQLConnect1;";
+
+            IDbConnection dbConnection = new SqlConnection(connectionString);
+            string sqlCommand = "SELECT GETDATE()";
+
+            DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+
+            Console.WriteLine(rightNow);
+
+            // // Declare an instance
+            // Computer myComputer = new Computer()
+            // {
+            //     Motherboard = "Z690",
+            //     HasWifi = true,
+            //     hasLTE = false,
+            //     ReleaseDate = DateTime.Now,
+            //     Price = 954.87m,
+            //     VideoCard = "TRX 2060"
+            // };
+            // myComputer.HasWifi = false;
+            // Console.WriteLine(myComputer.Motherboard);
+            // Console.WriteLine(myComputer.HasWifi);
+            // Console.WriteLine(myComputer.ReleaseDate);
+            // Console.WriteLine(myComputer.VideoCard);
+
         }
     }
 }
